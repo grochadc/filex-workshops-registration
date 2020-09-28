7917dccf48823a4ef94e43669e7dad93495180f0
Gonzalo Rocha <grochadc@gmail.com>
2020-09-28 18:30:22 -0500

    Delete unnecessary useFetch hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,53 +1,4 @@
-import { useEffect, useState } from "react";
-
-type FetchError = {
-  status: number;
-  message: string;
-};
-
-type ResType = {
-  ok: boolean;
-  status: number;
-};
-
-const useFetch = (url: string) => {
-  const [response, setResponse] = useState({ name: "", code: "" });
-  const [error, setError] = useState<FetchError>();
-  const [isLoading, setIsLoading] = useState(false);
-  const [res, setRes] = useState<ResType | undefined>();
-  useEffect(() => {
-    let mounted = true;
-
-    const fetchData = () => {
-      setIsLoading(true);
-      fetch(url)
-        .then((res) => {
-          setRes({ ok: res.ok, status: res.status });
-          res.json();
-        })
-        .then((json: any) => {
-          if (mounted && res && res.ok) {
-            setResponse(json);
-            setIsLoading(false);
-          } else if (res && res.ok === false) {
-            setError({
-              status: res.status,
-              message:
-                res.status === 404 ? "Not found" : `Error status ${res.status}`,
-            });
-          }
-        })
-        .catch(() => setError({ status: 500, message: "Network error" }));
-    };
-    fetchData();
-    function callback() {
-      mounted = false;
-    }
-    return callback;
-  }, [url]);
-  return { response, error, isLoading };
-};
-
+import { useState } from "react";
 const useModal = () => {
   const [showModal, setShowModal] = useState(false);
   const handleCloseModal = () => setShowModal(false);
@@ -55,4 +6,4 @@ const useModal = () => {
   return { showModal, handleCloseModal, handleShowModal };
 };
 
-export { useFetch, useModal };
+export { useModal };
